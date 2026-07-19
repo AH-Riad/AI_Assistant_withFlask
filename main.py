089ffc37a26d832e3e62449188b32dfac7fa9b75
Riad <riadk910@gmail.com>
2026-07-19 22:06:09 +0600

    ask route

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, jsonify, render_template, request
 from flask.cli import load_dotenv
 import os
 from groq import Groq
@@ -15,11 +15,28 @@ def hello_world():
     return render_template("index.html")
 
 
+@app.route("/ask", methods=["POST"])
+def ask():
+    question = request.form.get('question')
+    
+    response = client.chat.completions.create(
+        model="llama-3.3-70b-versatile",
+        messages=[
+            { "role": "system", "content": "You are a helpful assistant." },
+            {
+                "role": "user",
+                "content": question
+            }],
+            temperature = 0.7,
+            max_tokens = 512
+        
+    )
 
+    answer = response.choices[0].message.content.strip()
 
-
-
-
+    return jsonify({
+        "response": answer
+    }),200
 
 
 
